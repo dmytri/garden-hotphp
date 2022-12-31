99e56828697fcf0af29ed7f1d80b904b3c8a61bb
Dmytri Kleiner <dk@trick.ca>
2022-12-31 15:12:09 +0100

    add cli

---
+++ site/app/cli.php
@@ -0,0 +1,6 @@
+<?php
+echo 'Component:' .  $_SERVER['argv'][1] . "\n";
+echo 'Function:' . $_SERVER['argv'][2] . "\n";
+echo 'Config:' . $_SERVER['argv'][3] . "\n";
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' . "\n";
+echo 'Change me.' . "\n";
